2e04e33d032cb79906d60803dc09d1031ee0dac7
giri-alopa <gaakula@alopasoft.com>
2020-06-19 20:36:35 +0530

    form added

--- public/index.js
+++ public/index.js
@@ -21,4 +21,34 @@ $(document).ready(function(){
     });
 });
 
-  
+document.getElementById('training').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = {
+        name : e.target.Fullname.value,
+        phone : e.target.phoneNumber.value,
+        email : e.target.exampleInputEmail1.value,
+        location : e.target.location.value,
+        experience: e.target.experience.value,
+        languages : {
+            pythonBeginner : e.target.pythonBeginner.checked,
+            pythonAdvanced : e.target.pythonAdvanced.checked,
+            pythonExpert : e.target.pythonExpert.checked,
+            DataScienceWithPython : e.target.DataScienceWithPython.checked,
+            SQLBeginner : e.target.SQLBeginner.checked,
+            SQLAdvanced : e.target.SQLAdvanced.checked,
+            CMBasic : e.target.CMBasic.checked,
+            CMAdvanced : e.target.CMAdvanced.checked,
+            CMDemo : e.target.CMDemo.checked,
+            HTML : e.target.HTML.checked,
+        },
+        preferredTime :{
+            weekday: e.target.Weekdaybatches.checked,
+            weekend: e.target.Weekendbatches.checked
+        },
+        optionalCourse: e.target.technical.value,
+        comments:e.target.Comments.value
+
+    }
+    console.log(data)
+
+})
